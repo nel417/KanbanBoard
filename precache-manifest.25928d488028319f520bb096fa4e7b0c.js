030156fb50a291810bcb857cf9673e4dbff98865
nicholas landreville <nl@nicholass-MacBook-Air.local>
2020-04-03 14:29:48 -0500

    Updates

---
+++ precache-manifest.25928d488028319f520bb096fa4e7b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cb72f151865d6c1fd9d9950b12ba24",
+    "url": "/KanbanBoard/index.html"
+  },
+  {
+    "revision": "74dfc95ac37a4594e5b9",
+    "url": "/KanbanBoard/static/css/main.de79d1c2.chunk.css"
+  },
+  {
+    "revision": "f90f718b4e808b30c424",
+    "url": "/KanbanBoard/static/js/2.7316c766.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KanbanBoard/static/js/2.7316c766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dfc95ac37a4594e5b9",
+    "url": "/KanbanBoard/static/js/main.55797938.chunk.js"
+  },
+  {
+    "revision": "512483eb98fa56122bef",
+    "url": "/KanbanBoard/static/js/runtime-main.72974d86.js"
+  }
+]);
